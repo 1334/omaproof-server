4f1df79d7bf84bc4d56b37d019b4155874ee6147
Frederik Hermans <frederik.hermans@hotmail.com>
2018-11-28 18:41:55 +0100

    refa: folder structure for resolvers

---
+++ src/resolvers/mutations/content/contentResolvers.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+const uuidv4 = require('uuid/v4');
+const {
+  verifyPostId,
+  verifyUserIdByContactNumbers,
+  verifyUserIsAdminById,
+  verifyUserIsInGroupById
+} = require('../helperfunctions/verifications');
+
+module.exports = {};
