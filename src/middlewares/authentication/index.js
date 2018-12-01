1e4b9bc6444826246fc3979aa0fe5b04f19a77eb
Frederik Hermans <frederik.hermans@hotmail.com>
2018-12-01 11:57:35 +0100

    token adjustment: passed through argument, if statement added to creategroup to allow for empty groups (besides admin)

--- src/middlewares/authentication/index.js
+++ src/middlewares/authentication/index.js
@@ -1,10 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 const userAuthentication = (resolve, root, args, context, info) => {
-  const authorization = context.request.get('Authorization');
-  if (!authorization) throw new Error('Invalid authorization');
-
-  const token = authorization.replace('Bearer ', '');
+  const token = args.token;
+  if (!token) throw new Error('Invalid authorization');
   const { userId } = jwt.verify(token, process.env.APP_SECRET);
   context.userId = userId;
   if (!userId) throw new Error('Invalid authorization');
@@ -12,10 +10,8 @@ const userAuthentication = (resolve, root, args, context, info) => {
 };
 
 const groupAuthentication = (resolve, root, args, context, info) => {
-  const authorization = context.request.get('Authorization');
-  if (!authorization) throw new Error('Invalid authorization');
-
-  const token = authorization.replace('Bearer ', '');
+  const token = args.token;
+  if (!token) throw new Error('Invalid authorization');
   const { userId, activeGroup } = jwt.verify(token, process.env.APP_SECRET);
   if (!userId || !activeGroup) throw new Error('Invalid authorization');
   context.userId = userId;
